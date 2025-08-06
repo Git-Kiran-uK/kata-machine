dc27e8b65079f24d31f6859e32c6e85549c2aae1
Kiran Kanchan <155144115+Git-Kiran-uK@users.noreply.github.com>
2025-08-06 16:16:06 +0530

    Update BubbleSort.ts

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -8,6 +8,13 @@ test("bubble-sort", function () {
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
 
-export default function bubbleSort(arr: number[]): number[]{
-    return []
+export default function bubbleSort(arr: number[]){
+    const len = arr.length;
+    for(let i = 0; i < len; i++){
+        for(let j = 0; j < len - 1 - i; j++){
+            if(arr[j] > arr[j + 1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+    }
 }
